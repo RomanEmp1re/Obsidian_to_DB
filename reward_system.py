f0cddcdb9e19703338f78213eb81e2718944f60e
romeo <nyxnyxnyx66@gmail.com>
2025-08-13 12:05:46 +0300

    Started work on habits rewards and added slept counter

--- reward_system.py
+++ reward_system.py
@@ -4,7 +4,26 @@ import os
 from config import DATA_DIR
 
 
+class HabitsList:
+    def __init__(self):
+        self.path = os.path.join(DATA_DIR, 'habits_list')
+
 class HabitRewards:
 
     def __init__(self):
-        info = pd.read_csv()
+        self.path = os.path.join(DATA_DIR, 'habits_rewards.csv')
+        self.rewards = pd.read_csv(DATA_DIR)
+
+    def add_reward(
+            self, 
+            habit,
+            min_value,
+            max_value,
+            valid_from = dt.date.today()
+    ):
+        self.rewards.concat({
+            'habit' : habit,
+            'min_value' : min_value,
+            'max_value' : max_value,
+            'valid_from' : valid_from
+        })
